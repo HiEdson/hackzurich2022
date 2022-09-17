f25e95ae1007cbfdd864fc02bb8d9ea096b2ec81
leon <ishimweleon@gmail.com>
2022-09-17 19:33:02 +0300

    added openai routes

--- backend/src/openai.js
+++ backend/src/openai.js
@@ -4,14 +4,14 @@ const { Configuration, OpenAIApi } = require("openai");
 const config = new Configuration({ apiKey: process.env.OPENAI_KEY });
 const openai = new OpenAIApi(config);
 
-(async () => {
-  const key = process.env;
-  let prompt = `"I am an experienced fullstack programmer with experience in building distributed systems". From the above professional summary make a list of jobs that candidate can take up`;
-  let d = await makePrompt(prompt);
-  let skills = d.split("-");
-  for (let skill of skills) console.log(skill);
-  //   console.log(d);
-})();
+// (async () => {
+//   const key = process.env;
+//   let prompt = `"I am an experienced fullstack programmer with experience in building distributed systems". From the above professional summary make a list of jobs that candidate can take up`;
+//   let d = await makePrompt(prompt);
+//   let skills = d.split("-");
+//   for (let skill of skills) console.log(skill);
+//   //   console.log(d);
+// })();
 
 async function makePrompt(prompt, max_tokens = 100) {
   const response = await openai.createCompletion({
@@ -26,6 +26,7 @@ async function makePrompt(prompt, max_tokens = 100) {
   return text
     .replace(/[^\w- ]/g, "")
     .replace(/\s+/, " ")
+    .replace(/\d/, "")
     .toLowerCase();
 }
 
--- backend/src/routes/skills.js
+++ backend/src/routes/skills.js
@@ -1,17 +1,48 @@
 const express = require("express");
 const { getCollection } = require("../utils");
 const { createHash } = require("crypto");
+const { makePrompt } = require("../openai");
 
 const router = express.Router();
 
 router.post("/fromsummary", async (req, res) => {
   const { summary } = req.body;
-  return res.json({ status: "done", skills: [] });
+  const _jobs = await makePrompt(
+    `As a career consultant make a list of jobs you would recommend to a student with the following summary on their resume: "${summary}":`
+  );
+  const _skills = await makePrompt(
+    `As a career consultant make a list of skills you think a student with the following summary possess: "${summary}":`
+  );
+  const skills = [];
+  for (let skill of _jobs.split("-")) {
+    skill = skill.trim();
+    if (skill.length >= 4) skills.push(skill.toLowerCase());
+  }
+  for (let skill of _skills.split("-")) {
+    skill = skill.trim();
+    if (skill.length >= 4) skills.push(skill.toLowerCase());
+  }
+  return res.json({ status: "done", skills: skills });
 });
 
 router.post("/fromproblem", async (req, res) => {
   const { problem } = req.body;
-  return res.json({ status: "done", skills: [] });
+  const _skills = await makePrompt(
+    `"${problem}" In order for me to be able to solve the above problem make a bulleted list of skills i shoud have:`
+  );
+  const _jobs = await makePrompt(
+    `"${problem}" Make a bulleted list of professionals who can solve the above problem:`
+  );
+  const skills = [];
+  for (let skill of _jobs.split("-")) {
+    skill = skill.trim();
+    if (skill.length >= 4) skills.push(skill.toLowerCase());
+  }
+  for (let skill of _skills.split("-")) {
+    skill = skill.trim();
+    if (skill.length >= 4) skills.push(skill.toLowerCase());
+  }
+  return res.json({ status: "done", skills: skills });
 });
 
 module.exports = router;
