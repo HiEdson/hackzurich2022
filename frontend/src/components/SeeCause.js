49d71084c4741ae3b82d22d260d78ba210a098be
Edson <edsoncasimiro264@gmail.com>
2022-09-18 07:35:47 +0300

    keep all cards the same size

--- frontend/src/components/SeeCause.js
+++ frontend/src/components/SeeCause.js
@@ -19,10 +19,10 @@ const SeeCauses = () => {
         <div className="row">
           {location?.state?.causes &&
             location?.state?.causes.map((cause) => (
-              <div className=" col-sm-6 mb-3">
+                <div className=" col-sm-6 mb-3 d-flex align-items-stretch">
                 <div
                   class="card w-100"
-                  style={{ backgroundColor: "rgb(182 224 232)" }}
+                  style={{ backgroundColor:"rgb(182 224 232)"}}
                 >
                   <div class="card-body">
                     <h4 class="card-title">{cause.title}</h4>
